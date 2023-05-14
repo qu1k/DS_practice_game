c2478dbe566ea090a5cbbbda8617e820bd0eadaa
qu1k <ighor-sidoruk@mail.ru>
2023-05-14 11:33:16 +0300

    initial commit

---
+++ game_v3.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+    
+
+def game_core_v3(number: int = 1) -> int:
+    """Сначала находим среднее значение от минимальной и максимальной границой, затем
+    в зависимости от полученного числа, больше оно или меньше нужного, отсекаем все что меньше или больше,
+    после чего находим снова среднее значение и так пока не дойдем до нужного числа.
+    Функция принимает загаданное число и возвращает число попыток
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+    count = 0
+    predict_min, predict_max = 1, 100
+    while True:
+        count += 1
+        predict = (predict_min + predict_max) // 2
+        if predict == number:
+            break
+        elif predict > number:
+            predict_max = predict - 1
+        else:
+            predict_min = predict + 1
+    return count
+
+print('Run benchmarking for game_core_v3: ', end='')
+score_game(game_core_v3)
